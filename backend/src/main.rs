251d4b773f1b5c825e269f7e2a54a534b5d2e716
Kyle Leonhard <leonhard.kyle@gmail.com>
2024-06-07 10:02:56 -0700

    Setup postgres connection

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,9 +5,12 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use std::sync::{Arc, Mutex};
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use std::sync::Arc;
 use tower_http::services::ServeFile;
 use uuid::Uuid;
+
+use std::time::Duration;
 
 #[derive(Clone, serde::Serialize)]
 struct Task {
@@ -21,24 +24,29 @@ struct NewTask {
     name: String,
 }
 
-struct TaskStore {
-    tasks: Vec<Task>,
-}
-
 struct AppState {
-    task_store: Mutex<TaskStore>,
+    pool: PgPool,
 }
 
 #[tokio::main]
 async fn main() {
-    let store = Arc::new(AppState {
-        task_store: Mutex::new(TaskStore { tasks: Vec::new() }),
-    });
+    // Connect to the Postgres database.
+    let db_connection_str =
+        std::env::var("DATABASE_URL").unwrap_or_else(|_| "postgresql://localhost".to_string());
+    format!("Connecting to database: {}", db_connection_str);
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("can't connect to database");
+
+    let state = Arc::new(AppState { pool: pool });
     let app = Router::new()
         .route("/task/list", get(list_tasks))
         .route("/task/create", post(create_task))
         .route_service("/", ServeFile::new("assets/index.html"))
-        .with_state(store);
+        .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -53,12 +61,39 @@ async fn create_task(
         id: Uuid::new_v4().to_string(),
         name: new_task.name,
     };
-    state.task_store.lock().unwrap().tasks.push(task.clone());
+
+    sqlx::query("INSERT INTO tasks(id, name) VALUES ($1, $2);")
+        .bind(&task.id)
+        .bind(&task.name)
+        .execute(&state.pool)
+        .await
+        .map_err(internal_error)?;
     Ok(Json(task))
 }
 
 async fn list_tasks(
     State(state): State<Arc<AppState>>,
 ) -> Result<Json<Vec<Task>>, (StatusCode, String)> {
-    Ok(Json(state.task_store.lock().unwrap().tasks.to_vec()))
+    let results: Vec<(String, String)> = sqlx::query_as("SELECT id, name from tasks")
+        .fetch_all(&state.pool)
+        .await
+        .map_err(internal_error)?;
+
+    let mut tasks = Vec::new();
+    for row in results {
+        tasks.push(Task {
+            id: row.0,
+            name: row.1,
+        })
+    }
+
+    Ok(Json(tasks))
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error` response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
